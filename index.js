30f22bd546a12d5c031e7d7044634601f1f4ab17
JoaquinGomezCabido <joaquingomezcabido@gmail.com>
2019-09-24 17:10:28 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+
+function saturdayFun(activity = "roller-skate") {
+	return `This Saturday, I want to ${activity}!`;
+}
+
+let mondayWork = function(activity = "go to the office") {
+	return `This Monday, I will ${activity}.`;
+};
+
+function wrapAdjective(visual = "*") {
+	return function(adjective = "special") {
+		return `You are ${visual}${adjective}${visual}!`;
+	};
+}
+
+let Calculator = {
+	add: function(first, second) {
+		return first + second;
+	},
+	subtract: function(first, second) {
+		return first - second;
+	},
+	multiply: function(first, second) {
+		return first * second;
+	},
+	divide: function(first, second) {
+		return first / second;
+	},
+};
+
+function actionApplyer(starting, array) {
+	if (array.length == 0) {
+		return starting;
+	} else {
+		let result = starting;
+		array.forEach(item => {
+			result = item(result);
+		});
+		return result;
+	}
+}
